f1c9eded93231c40590d6563809c86cccd214ad3
Kevin Cox <kevin@outcastpixel.com>
2019-03-08 20:20:39 -0600

    Completed
    Completed and tested Caps Lock Indicator.

---
+++ capslock/CapsLock_Indicator.js
@@ -0,0 +1,11 @@
+/*
+**
+**
+*/
+CapsLock_Indicator = function() {
+    this.inputElementId = elementId;
+    this.capsLockOnStyleClass = 
+    this.capsLockOffStyleClass =
+    this.capsLockState =
+};
+
